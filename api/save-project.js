08008574ecb278008d579a27cd1ee0b4d910c070
jimbo <datos.electricidad.0000@gmail.com>
2025-10-05 12:03:56 +0200

    Update save-project.js

--- api/save-project.js
+++ api/save-project.js
@@ -2,7 +2,7 @@ const { Octokit } = require("@octokit/rest");
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const OWNER = "datoselectricidad";
-const REPO = "anotaciones";
+const REPO = "gestor-proyectos-visuales";
 const BRANCH = "main";
 const FOLDER = "proyectos";
 
